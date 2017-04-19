faf38663116fe2dbb31062d4f7ab7aa40091689b
Dheeraj Joshi <djosh1992@ccis.neu.edu>
2017-04-19 18:05:51 -0400

    commit v1

--- src/main/scala/MRProject/App.scala
+++ src/main/scala/MRProject/App.scala
@@ -1,9 +1,10 @@
 package MRProject
 
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
-import org.apache.spark.mllib.util.MLUtils
-
+import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel, SVMModel, SVMWithSGD}
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
 /**
   * @author ${user.name}
   */
@@ -23,24 +24,54 @@ object App {
 
     //Spark Context
     val sc = new SparkContext(conf)
-    val input = sc.textFile(args(0)).map(line => line.split(",")).map(x => (x(0),x(1),x(26),x(955)
-      ,x(956) ,x(957) ,x(958) , x(959), x(960),x(962),x(963),x(966),x(967)))
-    val filtered = input.filter(x => (!x._3.equals("0") || x._3.equalsIgnoreCase("X")))
-    filtered.repartition(1)
-      .saveAsTextFile(args(1));
+    val input = sc.textFile(args(0)).map(line => line.split(",")).map(x => (if(x(26).toInt > 0 || x(26).equals("X")){1}else{0},List(
+      x(955).toDouble,
+      x(956).toDouble,
+      x(957).toDouble,
+      x(959).toDouble,
+      x(962).toDouble,
+      x(963).toDouble,
+      x(966).toDouble,
+      x(967).toDouble)))
+    val parsed = input.map { case (k, vs) =>
+      LabeledPoint(k.toDouble, Vectors.dense(vs.toArray))
+    }
+
+    /*filtered.repartition(1)
+      .saveAsTextFile(args(1));*/
 
     //val data = MLUtils.loadLibSVMFile(sc, "data/mllib/sample_libsvm_data.txt")
 
     // Split data into training (60%) and test (40%).
-    val Array(training, test) = filtered.randomSplit(Array(0.6, 0.4))
+    //val Array(training, test) = filtered.randomSplit(Array(0.6, 0.4))
 
-    val model = NaiveBayes.train(training, lambda = 1.0, modelType = "multinomial")
 
-    val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
-    val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
+    // Split data into training (60%) and test (40%).
+    val splits = parsed.randomSplit(Array(0.6, 0.4), seed = 11L)
+    val training = splits(0).cache()
+    val test = splits(1)
+
+    // Run training algorithm to build the model
+    val numIterations = 100
+    val model = SVMWithSGD.train(training, numIterations)
+
+    // Clear the default threshold.
+    model.clearThreshold()
+
+    // Compute raw scores on the test set.
+    val scoreAndLabels = test.map { point =>
+      val score = model.predict(point.features)
+      (score, point.label)
+    }
+
+    // Get evaluation metrics.
+    val metrics = new BinaryClassificationMetrics(scoreAndLabels)
+    val auROC = metrics.areaUnderROC()
+
+    println("Area under ROC = " + auROC)
 
     // Save and load model
-    model.save(sc, "target/tmp/myNaiveBayesModel")
-    val sameModel = NaiveBayesModel.load(sc, "target/tmp/myNaiveBayesModel")
+    model.save(sc, "target/tmp/scalaSVMWithSGDModel")
+    val sameModel = SVMModel.load(sc, "target/tmp/scalaSVMWithSGDModel")
   }
 }
